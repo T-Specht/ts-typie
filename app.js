e0217fd4f0126ac4cfc376e4093b17faa7644531
Tim Specht <dev@tim-specht.com>
2017-07-22 16:51:16 +0200

    Fixed errors with tsc

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var path = require("path");
 var child_process_1 = require("child_process");
