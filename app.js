487e28c43d6998233621a7b9655d7c296b591c98
GarmOfGnipahellir <melsom.henrik@gmail.com>
2019-02-19 15:12:47 +0100

    version 2.0.0 - can change package manager tool - refactored some style breaking code

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ var child_process_1 = require("child_process");
 var request = require("request");
 var chalk = require("chalk");
 var figures = require("figures");
+var args = require("args");
+var commandExists = require("command-exists");
+// list of supported package manager tools
+// the first one found will be default
+var tools = {
+    yarn: { command: 'yarn add -D' },
+    npm: { command: 'npm install -D' }
+};
+// look for the first available tool
+var defaultTool;
+for (var _i = 0, _a = Object.keys(tools); _i < _a.length; _i++) {
+    var tool_1 = _a[_i];
+    if (commandExists.sync(tool_1)) {
+        defaultTool = tool_1;
+        break;
+    }
+}
+if (defaultTool === undefined) {
+    console.error('Couldn\'t find a supported package manager tool.');
+}
+// support for overriding default
+args.option('tool', 'Which package manager tool to use', defaultTool);
+var opts = args.parse(process.argv, {
+    name: 'ts-typie',
+    mri: undefined,
+    mainColor: 'yellow',
+    subColor: 'dim'
+});
+var tool = tools[opts.tool];
+// check if package.json exists
 var cwd = process.cwd();
 var packagePath = path.join(cwd, 'package.json');
 if (!fs.existsSync(packagePath)) {
@@ -46,7 +76,7 @@ var _loop_1 = function (dependency) {
     (function (dependency) {
         request('https://www.npmjs.com/package/@types/' + dependency, function (err, res, body) {
             if (res.statusCode == 200) {
-                child_process_1.exec("npm install -D @types/" + dependency, function (err, stdout, stderr) {
+                child_process_1.exec(tool.command + " @types/" + dependency, function (err, stdout, stderr) {
                     if (!err) {
                         console.log(chalk.green(figures.tick, "@types/" + dependencyString + " installed successfully!"));
                     }
@@ -58,7 +88,7 @@ var _loop_1 = function (dependency) {
         });
     })(dependency);
 };
-for (var _i = 0, dependencies_1 = dependencies; _i < dependencies_1.length; _i++) {
-    var dependency = dependencies_1[_i];
+for (var _b = 0, dependencies_1 = dependencies; _b < dependencies_1.length; _b++) {
+    var dependency = dependencies_1[_b];
     _loop_1(dependency);
 }
--- app.ts
+++ app.ts
@@ -1,11 +1,46 @@
 #!/usr/bin/env node
-import * as fs from "fs";
-import * as path from "path";
-import { exec } from "child_process";
-import * as request from "request";
-import * as chalk from "chalk";
-import * as figures from "figures";
+import * as fs from 'fs';
+import * as path from 'path';
+import { exec } from 'child_process';
+import * as request from 'request';
+import * as chalk from 'chalk';
+import * as figures from 'figures';
+import * as args from 'args';
+import * as commandExists from 'command-exists';
 
+
+// list of supported package manager tools
+// the first one found will be default
+const tools = {
+    yarn: { command: 'yarn add -D' },
+    npm: { command: 'npm install -D' }
+};
+
+// look for the first available tool
+let defaultTool;
+for (const tool of Object.keys(tools)) {
+    if (commandExists.sync(tool)) {
+        defaultTool = tool;
+        break;
+    }
+}
+if (defaultTool === undefined) {
+    console.error('Couldn\'t find a supported package manager tool.')
+}
+
+// support for overriding default
+args.option('tool', 'Which package manager tool to use', defaultTool);
+const opts = args.parse(process.argv, {
+    name: 'ts-typie',
+    mri: undefined,
+    mainColor: 'yellow',
+    subColor: 'dim'
+});
+const tool = tools[opts.tool];
+
+
+
+// check if package.json exists
 
 const cwd = process.cwd();
 const packagePath = path.join(cwd, 'package.json');
@@ -33,22 +68,22 @@ let alreadyInstalledTypes = dependencies.filter(d => /^@types\//.test(d));;
 dependencies = dependencies.filter(d => !/^@types\//.test(d));
 
 for (let dependency of dependencies) {
-     const dependencyString = chalk.bold(dependency)
+    const dependencyString = chalk.bold(dependency)
 
     // Check if types are already installed
 
-    if(alreadyInstalledTypes.includes('@types/' + dependency)){
+    if (alreadyInstalledTypes.includes('@types/' + dependency)) {
         console.log(chalk.yellow(figures.play, `Types for ${dependencyString} already installed. Skipping...`));
         continue;
     }
 
     // Check for included types
     let pkgPath = path.join(cwd, 'node_modules', dependency, 'package.json');
-   
 
-    if(fs.existsSync(pkgPath)){
+
+    if (fs.existsSync(pkgPath)) {
         const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-        if(pkg.types || pkg.typings){
+        if (pkg.types || pkg.typings) {
             console.log(chalk.yellow(figures.warning, `Module ${dependencyString} includes own types. Skipping...`));
             continue;
         }
@@ -56,20 +91,20 @@ for (let dependency of dependencies) {
 
     // Check if types are available    
 
-     ((dependency) => {
+    ((dependency) => {
         request('https://www.npmjs.com/package/@types/' + dependency, (err, res, body) => {
-            
-            if(res.statusCode == 200){
-                exec(`npm install -D @types/${dependency}`, (err, stdout, stderr) => {
-                    if(!err){
+
+            if (res.statusCode == 200) {
+                exec(`${tool.command} @types/${dependency}`, (err, stdout, stderr) => {
+                    if (!err) {
                         console.log(chalk.green(figures.tick, `@types/${dependencyString} installed successfully!`));
                     }
                 });
-            }else{
+            } else {
                 console.log(chalk.red(figures.cross, `No types found for ${dependencyString} in registry. Skipping...`));
             }
 
         });
-    })(dependency); 
-    
+    })(dependency);
+
 }
